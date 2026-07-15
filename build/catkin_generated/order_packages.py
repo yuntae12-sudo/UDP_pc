7e5c7bdde54dc6d9338b9d1349bf08c54aaa069e
yuntae12-sudo <agape0842@gmail.com>
2026-07-15 20:39:49 +0900

    Initial workspace setup

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/autonav/udp_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/autonav/team_3/devel;/opt/ros/noetic'.split(';') if '/home/autonav/team_3/devel;/opt/ros/noetic' != '' else []
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/autonav/udp_ws/devel:$CMAKE_PREFIX_PATH"
+export PATH='/opt/ros/noetic/bin:/home/autonav/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/usr/lib/wsl/lib:/mnt/c/Users/dydwn/anaconda3:/mnt/c/Users/dydwn/anaconda3/Library/mingw-w64/bin:/mnt/c/Users/dydwn/anaconda3/Library/usr/bin:/mnt/c/Users/dydwn/anaconda3/Library/bin:/mnt/c/Users/dydwn/anaconda3/Scripts:/mnt/c/Users/dydwn/anaconda3/bin:/mnt/c/Users/dydwn/anaconda3/condabin:/mnt/c/WINDOWS/system32:/mnt/c/WINDOWS:/mnt/c/WINDOWS/System32/Wbem:/mnt/c/WINDOWS/System32/WindowsPowerShell/v1.0:/mnt/c/WINDOWS/System32/OpenSSH:/mnt/c/Program Files/Git/cmd:/mnt/c/Program Files/Docker/Docker/resources/bin:/mnt/c/Program Files/dotnet:/mnt/c/Users/dydwn/AppData/Local/Microsoft/WindowsApps:/mnt/c/Users/dydwn/AppData/Local/Programs/Microsoft VS Code/bin:/snap/bin'
+export PWD='/home/autonav/udp_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/autonav/udp_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/autonav/udp_ws/src:$ROS_PACKAGE_PATH"
